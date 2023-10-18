11dfa71c717d75d1744cfd53d783c9e4f83e46b3
felixevers5754 <141927502+felixevers5754@users.noreply.github.com>
2023-10-18 13:59:39 +0200

    Improved Turn System

--- X_O_game.py
+++ X_O_game.py
@@ -5,9 +5,10 @@ class x_o:
         self.a = [' ',' ',' ',' ',' ',' ',' ',' ',' '] 
         self.show()
         self.free = 9
+        self.player = 0
         while self.free:
             if self.check()!=0:
-                self.step_x()
+                self.turn()
                 self.free-=1
                 if self.check()==0:
                     print ('"X" Wins!!!!')
@@ -19,7 +20,7 @@ class x_o:
                 print ('"O" Wins!!!!')
                 break
             if self.check()!=0:
-                self.step_o()
+                self.turn()
                 self.free-=1
             else:
                 print ('"X" Wins!!!!')
@@ -34,24 +35,41 @@ class x_o:
              print(f"| {self.a[i]}",end=' |\n' if i%3==2 else ' ')
              if (i+1)%3==0:
                 print("+---"*3+'+')
-        
-    def step_x(self):
-        x = int(input("\nX`s step: "))
-        if x==0 or x > 9 > 0 or self.a[x-1] !=' ':
-            print ("Error\nTry again")
-            self.step_x()
-        else:
-            self.a[x-1] = 'X'
-        self.show()
 
-    def step_o(self):
-        x = int(input("\nO`s step: "))
-        if   x==0 or x > 9 > 0 or self.a[x-1] !=' ':
-            print ("Error\nTry again:")
-            self.step_o()
-        else:
-            self.a[x-1] = 'O'
+    def turn(self):
+        player_character = 'O' if self.player else 'X'
+        pos = input(f"{player_character}'s turn: ")
+
+        if not pos.isdigit():
+            print("Error! Try Again!")
+            self.turn
+
+        pos = int(pos)
+
+        if pos not in range(1, 10):
+            print("Error! Try Again!")
+
+        self.a[pos - 1] = player_character
         self.show()
+        self.player = not self.player
+        
+    # def step_x(self):
+    #     x = int(input("\nX`s step: "))
+    #     if x==0 or x > 9 > 0 or self.a[x-1] !=' ':
+    #         print ("Error\nTry again")
+    #         self.step_x()
+    #     else:
+    #         self.a[x-1] = 'X'
+    #     self.show()
+
+    # def step_o(self):
+    #     x = int(input("\nO`s step: "))
+    #     if   x==0 or x > 9 > 0 or self.a[x-1] !=' ':
+    #         print ("Error\nTry again:")
+    #         self.step_o()
+    #     else:
+    #         self.a[x-1] = 'O'
+    #     self.show()
 
     def check(self):
         if self.a[0] == self.a[1] == self.a[2] != " " or  self.a[0] == self.a[4] == self.a[8] !=' ' or self.a[0] == self.a[3] == self.a[6] !=" ":
